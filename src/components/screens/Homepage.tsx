bb71487845abb80e61500f9771703814cdf5d673
Yana <zubalana0@gmail.com>
2026-05-28 06:58:13 +0300

    place name, qualificatiion & social media icons on the homepage, add a glowing hover effect to the icons

--- src/components/screens/Homepage.tsx
+++ src/components/screens/Homepage.tsx
@@ -1,21 +1,44 @@
+// import { motion } from 'framer-motion'
 import Navigation from "../functionalElements/Navigation";
 import HomepageBg from "../decorations/HomepageBg";
 import portfolioPhoto from "../../assets/portfolio_photo_cropped.png";
-import tailwindIcon from "../../assets/techIcons/tailwind.svg"
-import reactIcon from "../../assets/techIcons/react.svg"
-import htmlIcon from "../../assets/techIcons/html.svg"
-import cssIcon from "../../assets/techIcons/css.svg"
-import figmaIcon from "../../assets/techIcons/figma.svg"
-import shopifyIcon from "../../assets/techIcons/shopify.svg"
+import tailwindIcon from "../../assets/techIcons/tailwind.svg";
+import reactIcon from "../../assets/techIcons/react.svg";
+import htmlIcon from "../../assets/techIcons/html.svg";
+import cssIcon from "../../assets/techIcons/css.svg";
+import figmaIcon from "../../assets/techIcons/figma.svg";
+import shopifyIcon from "../../assets/techIcons/shopify.svg";
 
-import typescriptIcon from "../../assets/techIcons/typescript.svg"
-import nodejsIcon from "../../assets/techIcons/nodeJS.svg"
-import mongoDBIcon from "../../assets/techIcons/mongoDB.svg"
-import javascriptIcon from "../../assets/techIcons/javascript.svg"
-import swaggerIcon from "../../assets/techIcons/swagger.svg"
-import gitIcon from "../../assets/techIcons/git.svg"
+import typescriptIcon from "../../assets/techIcons/typescript.svg";
+import nodejsIcon from "../../assets/techIcons/nodeJS.svg";
+import mongoDBIcon from "../../assets/techIcons/mongoDB.svg";
+import javascriptIcon from "../../assets/techIcons/javascript.svg";
+import swaggerIcon from "../../assets/techIcons/swagger.svg";
+import gitIcon from "../../assets/techIcons/git.svg";
+
+import githubIcon from "../../assets/socialMedia/github.svg";
+import instIcon from "../../assets/socialMedia/inst.svg";
+import telegramIcon from "../../assets/socialMedia/telegram.svg";
 
 export default function Homepage() {
+  const socialMedia = [
+    {
+      icon: githubIcon,
+      alt: "Github icon",
+      link: "https://github.com/ZubalYana",
+    },
+    {
+      icon: telegramIcon,
+      alt: "Telegram icon",
+      link: "https://t.me/yanavesq#",
+    },
+    {
+      icon: instIcon,
+      alt: "Instagram icon",
+      link: "https://www.instagram.com/yanavesq/",
+    },
+  ];
+
   return (
     <div className="w-full md:h-screen p-[20px] lg:p-[40px]">
       <HomepageBg />
@@ -28,19 +51,98 @@ export default function Homepage() {
             className="lg:w-[350px] lg:h-[515px] opacity-[0.9]"
           />
           <div>
-            <img src={tailwindIcon} alt="tailwind icon" className="absolute top-6 left-80 rotate-[-15deg] w-16 h-16" />
-            <img src={reactIcon} alt="react icon" className="absolute top-32 left-85 rotate-[20deg] w-18 h-18" />
-            <img src={htmlIcon} alt="html icon" className="absolute top-45 left-50 rotate-[-8deg] w-15 h-15"  />
-            <img src={cssIcon} alt="css icon" className="absolute top-65 left-80 rotate-[23deg] w-13 h-13"  />
-            <img src={figmaIcon} alt="figma icon" className="absolute top-81 left-56 rotate-[3deg] w-14 h-14" />
-            <img src={shopifyIcon} alt="shopify icon" className="absolute top-100 left-80 rotate-[3deg] w-14 h-14" />
+            {/* <motion.img
+  initial={{ opacity: 0, y: 30 }}
+  whileInView={{ opacity: 1, y: 0 }}
+  viewport={{ once: true }}
+  transition={{ duration: 0.5, delay: 0.1 }}
+  src={reactIcon}
+/> */}
+            <img
+              src={tailwindIcon}
+              alt="tailwind icon"
+              className="absolute top-6 left-80 rotate-[-15deg] w-16 h-16"
+            />
+            <img
+              src={reactIcon}
+              alt="react icon"
+              className="absolute top-32 left-85 rotate-[20deg] w-18 h-18"
+            />
+            <img
+              src={htmlIcon}
+              alt="html icon"
+              className="absolute top-45 left-50 rotate-[-8deg] w-15 h-15"
+            />
+            <img
+              src={cssIcon}
+              alt="css icon"
+              className="absolute top-65 left-80 rotate-[23deg] w-13 h-13"
+            />
+            <img
+              src={figmaIcon}
+              alt="figma icon"
+              className="absolute top-81 left-56 rotate-[3deg] w-14 h-14"
+            />
+            <img
+              src={shopifyIcon}
+              alt="shopify icon"
+              className="absolute top-100 left-80 rotate-[3deg] w-14 h-14"
+            />
 
-            <img src={typescriptIcon} alt="typescript icon" className="absolute top-10 left-205 rotate-[-10deg] w-15 h-15" />
-            <img src={nodejsIcon} alt="nodejs icon" className="absolute top-33 left-185 rotate-[3deg] w-14 h-14" />
-            <img src={mongoDBIcon} alt="mongoDB icon" className="absolute top-47 left-225 rotate-[15deg] w-16 h-16" />
-            <img src={javascriptIcon} alt="javascript icon" className="absolute top-65 left-200 rotate-[20deg] w-14 h-14" />
-            <img src={swaggerIcon} alt="swagger icon" className="absolute top-90 left-230 rotate-[-20deg] w-12 h-12" />
-            <img src={gitIcon} alt="git icon" className="absolute top-100 left-190 rotate-[-8deg] w-15 h-15" />
+            <img
+              src={typescriptIcon}
+              alt="typescript icon"
+              className="absolute top-10 left-205 rotate-[-10deg] w-15 h-15"
+            />
+            <img
+              src={nodejsIcon}
+              alt="nodejs icon"
+              className="absolute top-33 left-185 rotate-[3deg] w-14 h-14"
+            />
+            <img
+              src={mongoDBIcon}
+              alt="mongoDB icon"
+              className="absolute top-47 left-225 rotate-[15deg] w-16 h-16"
+            />
+            <img
+              src={javascriptIcon}
+              alt="javascript icon"
+              className="absolute top-65 left-200 rotate-[20deg] w-14 h-14"
+            />
+            <img
+              src={swaggerIcon}
+              alt="swagger icon"
+              className="absolute top-90 left-230 rotate-[-20deg] w-12 h-12"
+            />
+            <img
+              src={gitIcon}
+              alt="git icon"
+              className="absolute top-100 left-190 rotate-[-8deg] w-15 h-15"
+            />
+          </div>
+          <div className="w-full absolute bottom-6 flex justify-between">
+            <div className="flex flex-col text-center md:text-left">
+              <h3 className="text-[24px] font-bold">Yana Zubal</h3>
+              <p className="text-[14px] font-light">
+                Full-stack web dev, UI/UX designer
+              </p>
+            </div>
+            <div className="flex items-center gap-x-6">
+              {socialMedia.map((media, index) => (
+                <a
+                  href={media.link}
+                  key={index}
+                  target="_blank"
+                  className="w-[30px] h-[30px] cursor-pointer"
+                >
+                  <img
+                    src={media.icon}
+                    alt={media.alt}
+                    className="w-full h-full icon-hover"
+                  />
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
